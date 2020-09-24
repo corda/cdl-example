22917709de4fb918c12bac28eea95a578b038e6e
mattbradburyr3 <matthew.bradbury@r3.com>
2020-09-24 09:42:50 +0100

    additional state constraints working, but naming confusing

--- contracts/src/main/kotlin/com/cdlexample/contracts/ContractUtils.kt
+++ contracts/src/main/kotlin/com/cdlexample/contracts/ContractUtils.kt
@@ -5,35 +5,36 @@ import com.cdlexample.states.StatusState
 import net.corda.core.contracts.*
 
 // todo: consider if these classes should be data classes or not
+// todo: should the constraints be passing error messages back rather than false?
+// todo: do these need to be locked to State Type?
 
 data class Path(val commandClass: Class<out CommandData>,
                 val outputStatus: Status?,
                 val numberOfInputStates: Int,
                 val numberOfOutputStates: Int,
-                val additionalInputs: Set<AdditionalStates> = setOf(),
-                val additionalOutputs: Set<AdditionalStates> = setOf())
+                val additionalInputs: Set<AdditionalStateType> = setOf(),
+                val additionalOutputs: Set<AdditionalStateType> = setOf())
 
-data class AdditionalStates(val type: Class<out ContractState>, val numberOfStates: Int)
+
+data class AdditionalStateType(val type: Class<out ContractState>, val numberOfStates: Int)
+
 
 class PathConstraint(val commandClass: Class<out CommandData>,
                      val outputStatus: Status?,
                      val multiplicityIn: Multiplicity = Multiplicity(),
                      val multiplicityOut: Multiplicity = Multiplicity(),
-
-                     // todo: these additionalInputs and outputs need to be able to specify a range of numbers of states, ie use a Multiplicity
-                     val additionalInputs: Set<AdditionalStates> = setOf(),
-                     val additionalOutputs: Set<AdditionalStates> = setOf()){
+                     val additionalInputsConstraints: AdditionalStatesConstraints = AdditionalStatesConstraints(),
+                     val additionalOutputsConstraints: AdditionalStatesConstraints = AdditionalStatesConstraints()){
 
     infix fun allows(p: Path): Boolean{
-            var match = true
-            if (commandClass != p.commandClass) match = false
-            if (outputStatus != p.outputStatus) match = false
-            if (multiplicityIn doesNotAllow p.numberOfInputStates) match = false
-            if (multiplicityOut doesNotAllow p.numberOfOutputStates) match = false
-
-            // todo: check all required additional states are present
-            if (match) return true
-            return false
+            var allows = true
+            if (commandClass != p.commandClass) allows = false
+            if (outputStatus != p.outputStatus) allows = false
+            if (multiplicityIn doesNotAllow p.numberOfInputStates) allows = false
+            if (multiplicityOut doesNotAllow p.numberOfOutputStates) allows = false
+            if (additionalInputsConstraints isNotSatisfiedBy p.additionalInputs) allows = false
+            if (additionalOutputsConstraints isNotSatisfiedBy p.additionalOutputs) allows = false
+            return allows
     }
 
     infix fun doesNotAllow(p: Path): Boolean {
@@ -41,6 +42,35 @@ class PathConstraint(val commandClass: Class<out CommandData>,
     }
 }
 
+data class AdditionalStatesConstraints(val constraints: Set<AdditionalStatesConstraint> = setOf()) {
+
+    infix fun isSatisfiedBy(additionalStatesType: Set<AdditionalStateType>): Boolean {
+        for (c in constraints){
+            var satisfied = false
+            for (s in additionalStatesType){
+                if (c isSatisfiedBy s) satisfied = true
+            }
+            if (!satisfied) return false
+        }
+        return true
+    }
+    infix fun isNotSatisfiedBy(additionalStatesType: Set<AdditionalStateType>): Boolean {
+        return !isSatisfiedBy(additionalStatesType)
+    }
+}
+
+data class AdditionalStatesConstraint(val type: Class<out ContractState>, val requiredNumberOfStates: Multiplicity) {
+
+    infix fun isSatisfiedBy(additionalStates: AdditionalStateType ):Boolean {
+        var match = true
+        if (type != additionalStates.type) return false
+        if (requiredNumberOfStates.doesNotAllow(additionalStates.numberOfStates)) return false
+        return true
+    }
+    infix fun isNotSatisfiedBy (additionalStates: AdditionalStateType): Boolean{
+        return !isSatisfiedBy(additionalStates)
+    }
+}
 
 data class Multiplicity(val from: Int = 1,
                         val bounded: Boolean = true,
--- contracts/src/test/kotlin/com/cdlexample/contracts/ContractUtilsTests.kt
+++ contracts/src/test/kotlin/com/cdlexample/contracts/ContractUtilsTests.kt
@@ -26,6 +26,9 @@ class ContractUtilsTests() {
     @BelongsToContract(TestContract::class)
     class TestStateC(override val participants: List<AbstractParty> = listOf()) : ContractState
 
+    @BelongsToContract(TestContract::class)
+    class TestStateD(override val participants: List<AbstractParty> = listOf()) : ContractState
+
 
     class TestContract : Contract {
         override fun verify(tx: LedgerTransaction) {
@@ -133,4 +136,88 @@ class ContractUtilsTests() {
         val path8 = Path(TestContract.Commands.Command1()::class.java, TestStateA.TestStatus.STATUSA1, 1, 3)
         assert(pathConstraint2 allows  path8)
     }
+
+    @Test
+    fun `check AdditionalStateConstraint`(){
+
+        val asc = AdditionalStatesConstraint(TestStateB::class.java, Multiplicity(1, false))
+
+        // Check state match
+        val as1 = AdditionalStateType(TestStateA::class.java, 1)
+        assert (asc isNotSatisfiedBy as1)
+
+        val as2 = AdditionalStateType(TestStateB::class.java, 1)
+        assert (asc isSatisfiedBy as2)
+
+        val as3 = AdditionalStateType(TestStateC::class.java, 1)
+        assert (asc isNotSatisfiedBy as3)
+
+        // Check Multiplicity match
+        val as4 = AdditionalStateType(TestStateB::class.java, 0)
+        assert (asc isNotSatisfiedBy as4)
+
+        val as5 = AdditionalStateType(TestStateB::class.java, 1)
+        assert (asc isSatisfiedBy as5)
+
+        val as6 = AdditionalStateType(TestStateB::class.java, 2)
+        assert (asc isSatisfiedBy as6)
+
+    }
+
+    @Test
+    fun `check AdditionalStateConstraints`(){
+
+
+        val asc1 = AdditionalStatesConstraint(TestStateB::class.java, Multiplicity(1, true, 2))
+        val asc2 = AdditionalStatesConstraint(TestStateC::class.java, Multiplicity(1, false))
+
+        val ascs = AdditionalStatesConstraints(setOf(asc1, asc2))
+
+
+        // check state type
+        val as1 = AdditionalStateType(TestStateB::class.java, 1)
+        val as2 = AdditionalStateType(TestStateC::class.java, 1)
+        val as3 = AdditionalStateType(TestStateD::class.java, 1)
+
+        val set1 = setOf(as1,as2)
+        assert( ascs isSatisfiedBy set1)
+
+        val set2 = setOf(as1,as2, as3)
+        assert( ascs isSatisfiedBy set2)
+
+        val set3 = setOf(as2, as3)
+        assert( ascs isNotSatisfiedBy set3)
+
+        // Check multiplicity
+        val as4 = AdditionalStateType(TestStateB::class.java, 1)
+        val as5 = AdditionalStateType(TestStateB::class.java, 3)
+        val as6 = AdditionalStateType(TestStateC::class.java, 1)
+
+        val set4 = setOf(as4,as6)
+        assert( ascs isSatisfiedBy set4)
+
+        val set5 = setOf(as4,as5)
+        assert( ascs isNotSatisfiedBy set5)
+    }
+
+    @Test
+    fun `check PathConstraints with additional input and output constraints`(){
+
+
+        val pathConstraint1 = PathConstraint(TestContract.Commands.Command1()::class.java, TestStateA.TestStatus.STATUSA1, additionalInputsConstraints = AdditionalStatesConstraints(setOf(AdditionalStatesConstraint(TestStateB::class.java, Multiplicity(1, true, 2)))))
+
+        val as1 = AdditionalStateType(TestStateB::class.java, 1)
+        val path1 = Path(TestContract.Commands.Command1()::class.java, TestStateA.TestStatus.STATUSA1, 1, 1, setOf(as1))
+        assert(pathConstraint1 allows  path1)
+
+        val as2 = AdditionalStateType(TestStateC::class.java, 1)
+        val path2 = Path(TestContract.Commands.Command1()::class.java, TestStateA.TestStatus.STATUSA1, 1, 1, setOf(as2))
+        assert(pathConstraint1 doesNotAllow  path2)
+
+        val path3 = Path(TestContract.Commands.Command1()::class.java, TestStateA.TestStatus.STATUSA1, 1, 1, setOf(as1,as2))
+        assert(pathConstraint1 allows  path3)
+
+    }
+
+
 }
